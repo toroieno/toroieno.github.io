c57a704f4b0e5b84513e606a99187b31efeac1a4
trien.nv <trien.nv195934@gmail.com>
2025-07-15 23:52:57 +0700

    e

--- assets/js/template.js
+++ assets/js/template.js
@@ -396,7 +396,7 @@
     minute = second * 60,
     hour = minute * 60,
     day = hour * 24;
-  let birthday = "July 22, 2025 00:00:00",
+  let birthday = "July 22, 2025 10:00:00",
     countDown = new Date(birthday).getTime(),
     x = setInterval(function () {
       let now = new Date().getTime(),
